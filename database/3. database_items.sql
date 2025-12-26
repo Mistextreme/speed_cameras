e5f3e6287ee4965bfec20011c99dda35de8ef260
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-26 18:29:31 +0000

    Upload: database/3. database_items.sql

---
+++ database/3. database_items.sql
@@ -0,0 +1,4 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('anti_radar', 'Anti-radar', 1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('mobile_camera', 'Mobile speed camera', 1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('racer_jammer_device', 'Racer Jammer', 1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('police_antiradar_checker', 'Police Anti Radar', 1, 0, 1);
